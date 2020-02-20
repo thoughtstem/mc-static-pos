8a1ab83aafba21787f27db4803800cf1ef9f685e
Stephen Foster <stephen@thoughtstem.com>
2020-02-20 08:35:23 -0800

    first

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "mc-static-pos")
+(define deps '("base"
+	       "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/mc-static-pos.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(stephen))
---
+++ scribblings/mc-static-pos.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[mc-static-pos
+                    racket/base]]
+
+@title{mc-static-pos}
+@author{stephen}
+
+@defmodule[mc-static-pos]
+
+Package Description Here
